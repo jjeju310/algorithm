3b89b8a24d0dde0e925ad307c34df30818cbe317
hheeseung <ssigu69@gmail.com>
2021-10-11 17:03:47 +0900

    Week 4

---
+++ Basic Java/P030.java
@@ -0,0 +1,26 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class P030 {
+    // 백준 10871
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int array[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        sc.close();
+
+        for(int i = 0; i < n; i++) {
+            if(array[i] < x) {
+                System.out.print(array[i] + " ");
+            }
+        }
+
+    }
+}
