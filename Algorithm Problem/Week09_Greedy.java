cd565c4e0b83c2e053f7364683e72c3f8c00fa5b
hheeseung <ssigu69@gmail.com>
2021-10-11 17:43:05 +0900

    Week 9 Greedy

---
+++ Algorithm Problem/Week09_Greedy.java
@@ -0,0 +1,19 @@
+package AlgorithmProblem;
+
+import java.util.*;
+
+class Week09_Greedy {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+
+        int min = 0;
+        for(int max = people.length-1; min <= max; max--) {
+            if(people[min] + people[max] <= limit) { // if문에 걸리지 않는다: 혼자 탄다
+                min++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
