a1556d97b930df074de1e4aa9f97fed04187ec90
hheeseung <ssigu69@gmail.com>
2021-10-11 20:14:15 +0900

    Week 9

---
+++ Basic Java/P044.java
@@ -0,0 +1,13 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class P044 {
+    // 백준 11654
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ch = sc.next().charAt(0);
+
+        System.out.print(ch);
+    }
+}
---
+++ Basic Java/P045.java
@@ -0,0 +1,21 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class P045 {
+    // 백준 11720
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String a = sc.next();
+        sc.close();
+
+        int sum = 0;
+
+        for(int i = 0; i < N; i++) {
+            sum += a.charAt(i)-'0';
+        }
+        System.out.print(sum);
+    }
+}
