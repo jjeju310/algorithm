6ae73c4132bb71c493a7a34c1bee3e1cc7c4d9dc
hheeseung <ssigu69@gmail.com>
2021-10-11 16:54:45 +0900

    Week 3

---
+++ Basic Java/P014.java
@@ -0,0 +1,23 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class P014 {
+    // 백준 2588
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int n1 = num2 / 100;
+        int n2 = (num2 % 100) / 10;
+        int n3 = num2 % 10;
+
+        System.out.println(num1 * n3);
+        System.out.println(num1 * n2);
+        System.out.println(num1 * n1);
+        System.out.println(num1 * num2);
+
+    }
+}
