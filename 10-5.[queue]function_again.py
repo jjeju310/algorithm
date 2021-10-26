48e4f3ea1170f274b6b2096ddb17a5f1b350172c
jean1042 <jean1042@naver.com>
2021-10-26 16:18:05 +0900

    10-5 queue function dev problem (solve again)

---
+++ 10-5.[queue]function_again.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    def add(n, k):
+        return n+k
+    answer=[]
+
+    queue_progresses = deque(progresses) # 작업 개수 리스트를 큐로 변환
+    queue_speed = deque(speeds) # 속도 리스트를 큐로 변환
+
+    while queue_progresses:
+        count = 0 # 배포할때마다의 기능 개수
+
+        # 기능 개발이 하나만 남은 경우 날짜와 상관없이 하나만 배포하니깐 answer=1을 추가하고 종료
+        if len(queue_progresses)==1:
+            answer.append(1)
+            break
+
+        # 개발 진행도
+        queue_progresses = deque(map(add, queue_progresses, queue_speed)) # speed + 진행도
+        print(f'queue_progress: {queue_progresses}')
+        
+        while queue_progresses:
+            if queue_progresses[0]>=100: # 100%가 넘어가면 pop
+                queue_progresses.popleft()
+                queue_speed.popleft()
+                count += 1
+            else:
+                break
+
+        if count != 0:
+            answer.append(count)
+        
+    return answer
