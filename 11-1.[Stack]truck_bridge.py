c7860856c17ff17276845d55a3d506623a60f74e
jean1042 <jean1042@naver.com>
2021-11-08 18:09:23 +0900

    refactor: line..

--- 11-1.[Stack]truck_bridge.py
+++ 11-1.[Stack]truck_bridge.py
@@ -5,13 +5,12 @@ def main(bridge_length, weight, truck_weights):
     while len(trucks_on_bridge)>0: 
         time += 1
         trucks_on_bridge.pop(0) 
-
         if(len(truck_weights)>0): # 자 이제 트럭을 올려보자
             if sum(trucks_on_bridge)+truck_weights[0] <= weight: 
                 trucks_on_bridge.append(truck_weights.pop(0))
             else: # weight를 넘어버린 경우? 아무것도 안올라갔으니 
                 trucks_on_bridge.append(0)  # 리스트에 0 추가
     return time
-    
+
 if __name__ == '__main__':
     main(2, 10, [7,4,5,6])
